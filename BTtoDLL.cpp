978d315448422b6f5c9d3a52e9544584c7d86afa
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-05 00:53:06 +0530

    Create BTtoDLL.cpp

---
+++ BTtoDLL.cpp
@@ -0,0 +1,17 @@
+Binary Tree to DOubly Linked List
+
+We need to do the inorder traversal of the tree & while doing that, we can simply maintain a previous pointer of the previously traversed node. And node changes the right child of the previous node to current node & left child of current node as previous.
+
+  Node* BTtoDLL(Node* root){
+    Node* prev = NULL;
+    if(root == NULL) return root;
+    Node* head = BTtoDLL(root->left);
+    if(prev == NULL) head = root;
+    else{
+      root->left = prev;
+      prev->right = root;
+    }
+    prev = root;
+  BTtoDLL(root->right);
+  return head;
+  }
