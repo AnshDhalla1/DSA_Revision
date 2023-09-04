ae4f10d126eaf178f99491b5658f1ad641e461df
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-04 19:20:09 +0530

    Update Left_view.cpp

--- Left_view.cpp
+++ Left_view.cpp
@@ -1,1 +1,19 @@
-
+vector<int> LeftView(Node* root){
+  vector<int> ans;
+  if(root == NULL) return ans;
+  queue<Node*> q;
+  q.push(root);
+  while(!q.empty()){
+    int s = q.size();
+    for(int i=0; i<s; i++){
+      Node* curr = q.top();
+      q.pop();
+      if(i == 0){
+        ans.push(curr->val);
+      }
+      if(curr->left != NULL) q.push(curr->left);
+      if(curr->right != NULL) q.push(curr->right);
+    }
+  }
+  return ans;
+}
