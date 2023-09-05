e5f3be4102de956f01c31b07cdae3b2cda621e29
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-05 16:10:16 +0530

    Update 2. PreOrder Traversal.cpp

--- 2. PreOrder Traversal.cpp
+++ 2. PreOrder Traversal.cpp
@@ -1,5 +1,20 @@
 //RECURSIVE
+class Solution {
+    vector<int> ans;
+private:
+    void preorder(TreeNode* root){
+        if(root==NULL) return;
+        ans.push_back(root->val);
+        preorder(root->left);
+        preorder(root->right);
+    }
 
+public: 
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return ans;
+    }
+};
 
 
 //ITERATIVE
