f1744482276dba1a3606ee96f2703b2bde604911
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-05 18:05:26 +0530

    Update 2. PreOrder Traversal.cpp

--- 2. PreOrder Traversal.cpp
+++ 2. PreOrder Traversal.cpp
@@ -18,3 +18,17 @@ public:
 
 
 //ITERATIVE
+void preorder(TreeNode* root){
+    stack<TreeNode*> st;
+    vector<int> ans;
+    
+    if(root == NULL) return;
+    st.push(root);
+    while(!st.empty()){
+        TreeNode* curr = root;
+        ans.push_back(curr->val);
+        st.pop();
+        if(curr->right != NULL) st.push(curr->right);
+        if(curr->left != NULL) st.push(curr->left);
+    }
+}
