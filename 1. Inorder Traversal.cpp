7438552cd6fb5edc27ac4753f1df977ce8887a39
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-05 16:10:07 +0530

    Update 1. Inorder Traversal.cpp

--- 1. Inorder Traversal.cpp
+++ 1. Inorder Traversal.cpp
@@ -1,4 +1,21 @@
 //RECURSIVE
+class Solution {
+     vector<int> ans;
+private:
+    void inorder(TreeNode* root){
+        if(root==NULL) return;
+
+        inorder(root->left);
+        ans.push_back(root->val);
+        inorder(root->right);
+    }
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        inorder(root);
+        return ans;
+    }
+};
 
 
 //ITERATIVE
