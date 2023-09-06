cc1b63daf7077dd37d04c25768d8778877dd168e
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-06 23:59:35 +0530

    Update 4. Print all Permutations.cpp

--- 1. Recursion/4. Print all Permutations.cpp
+++ 1. Recursion/4. Print all Permutations.cpp
@@ -1,1 +1,19 @@
+I/p: s = "ABC"
+O/p: "ABC", "ACB", "BAC", "BCA", "CAB", "CBA"
 
+//Approach
+// We are going to fix every character at the 1st position & we are going to generate all permutations beginning with that character
+  
+  vector<string> permute(string &s, int i=0){
+    vector<string> v;
+    if(i == s.length()-1){
+      v.push_back(s);
+    }
+
+    for(int j =i; j<s.length(); j++){
+      swap(s[i], s[j]);
+      permute(s, i+1);
+      swap(s[j], s[i]);     //to get original string back
+    }
+    return v;
+  }
