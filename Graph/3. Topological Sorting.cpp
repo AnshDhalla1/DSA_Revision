3a34246fb3da9cf2b9e6826548ca86c17dfcae8e
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-13 00:46:20 +0530

    Update 3. Topological Sorting.cpp

--- Graph/3. Topological Sorting.cpp
+++ Graph/3. Topological Sorting.cpp
@@ -1,1 +1,6 @@
+/*
+- Valid for DAG(Directed Acyclic Graphs) only
 
+Linear ordering of vertices such that if there is an edge b/w 'u' & 'v', then 'u' appears before 'v' in that ordering
+TC = O(V+E)
+*/
