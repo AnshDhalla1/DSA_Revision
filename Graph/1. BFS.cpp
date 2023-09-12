22ffb53f3b5ef3b4c55561b631799dd781020559
Ansh Dhalla <83045012+AnshDhalla1@users.noreply.github.com>
2023-09-13 00:31:33 +0530

    Create 1. BFS.cpp

---
+++ Graph/1. BFS.cpp
@@ -0,0 +1,20 @@
+vector<int> BFS(int v, vector<int> adj){
+  vector<int> vis(0);
+  vector<int> trav;
+  vis[0] =1;
+  queue<int> q;
+  q.push(0);
+
+  while(!q.empty()){
+    int node = q.front();
+    q.pop();
+    trav.push_back(node);
+    for(auto it : adj[node]){
+      if(!vis[it]){
+        vis[it] =1;
+        q.push(it);
+      }
+    }
+  }
+  return trav
+}
